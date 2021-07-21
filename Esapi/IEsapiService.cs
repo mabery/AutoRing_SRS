1d4a3e82dc363c1f5f77e4b320c7a91810220303
Landon Clark <landon.d.clark@gmail.com>
2021-07-21 15:42:56 -0600

    Update IEsapiService.cs

--- Esapi/IEsapiService.cs
+++ Esapi/IEsapiService.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace AutoRingSRS
 {
@@ -6,6 +6,7 @@ namespace AutoRingSRS
     {
         Task<StructSet[]> GetStructureSetsAsync();
         Task<Struct[]> GetStructureIdsAsync(string structureSet, string keyword);
+        Task<string> GetEditableRingNameAsync(string structureSetId, string ringId);
         Task AddRingAsync(string structureSetId, string ptvId, string ringInnerId, string ringMiddleId, string ringOuterId, double innerMargin, double middleMargin, double outerMargin);
     }
 }
