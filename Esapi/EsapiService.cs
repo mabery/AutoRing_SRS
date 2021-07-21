af1d7f2f75e018be1b231ee0aca74c2de70e060a
Landon Clark <landon.d.clark@gmail.com>
2021-07-21 15:42:13 -0600

    Update EsapiService.cs

--- Esapi/EsapiService.cs
+++ Esapi/EsapiService.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using EsapiEssentials.Plugin;
 using VMS.TPS.Common.Model.API;
@@ -32,16 +33,58 @@ namespace AutoRingSRS
         public Task<Struct[]> GetStructureIdsAsync(string structureSet, string keyword) =>
             RunAsync(context =>
             {
-                return context.Patient.StructureSets?
-                .FirstOrDefault(x => x.Id == structureSet)
-                .Structures.Where(x => x.Id.ToUpper().Contains(keyword.ToUpper()) == true).Select(x => new Struct
+                var array = context.Patient.StructureSets?
+                        .FirstOrDefault(x => x.Id == structureSet)
+                        .Structures.Where(x => x.Id.ToUpper().Contains(keyword.ToUpper()) == true).Select(x => new Struct
+                        {
+                            StructureId = x.Id,
+                            StructureVolume = x.Volume,
+                            CanModify = Helpers.CheckStructure(x),
+                            IsHighRes = x.IsHighResolution
+                        })
+                        .ToArray();
+                if (keyword.Contains("Ring"))
                 {
-                    StructureId = x.Id,
-                    StructureVolume = x.Volume,
-                    CanModify = Helpers.CheckStructure(x)
-                })
-                .ToArray();
+                    Struct[] newArray = new Struct[array.Length + 1];
+                    newArray[0] = new Struct
+                    {
+                        StructureId = "<Create new structure>",
+                        CanModify = true,
+                        IsHighRes = true
+                    };
+                    Array.Copy(array, 0, newArray, 1, array.Length);
+                    return newArray;
+                }
+                return array;
             });
+
+        public Task<string> GetEditableRingNameAsync(string structureSetId, string ringId) =>
+            RunAsync(context => GetEditableRingName(context.Patient, structureSetId, ringId));
+
+        public string GetEditableRingName(Patient patient, string structureSetId, string ringId)
+        {
+            StructureSet structureSet = patient.StructureSets.FirstOrDefault(x => x.Id == structureSetId);
+            string possibleStructureId = String.Empty;
+
+            if (structureSet.Structures.Any(x => x.Id == ringId) == false) //Ring isnt present
+            {
+                return structureSet.Structures.Where(x => x.Id == ringId).FirstOrDefault().Id;
+            }
+
+            for (int i = 1; i <= 5; i++)
+            {
+                foreach (var structure in structureSet.Structures)
+                {
+                    if (structure.Id == ringId + i.ToString())  //possible already present
+                    {
+                        possibleStructureId = ringId + (i + 1).ToString();
+                        break;
+                    }
+                }
+            }
+            return possibleStructureId;
+            throw new Exception("Too many uneditable rings in structure set.");
+        }
 
         public Task AddRingAsync(string structureSetId, string ptvId, string ringInnerId, string ringMiddleId, string ringOuterId, double innerMargin, double middleMargin, double outerMargin) =>
             RunAsync(context => AddRing(context.Patient, structureSetId, ptvId, ringInnerId, ringMiddleId, ringOuterId, innerMargin, middleMargin, outerMargin));
